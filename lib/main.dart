e263aca944fc133f96c700c777740410580c3fab
Andy Mean <andymean8@gmail.com>
2025-02-12 22:26:33 -0500

    Added pulsing heart effect

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 178, 35, 35),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Heartbeat Animation'),
     );
   }
 }
@@ -29,27 +30,54 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController motionController;
+  double size = 100;
+
+  @override
+  void initState() {
+    super.initState();
+
+    motionController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+      lowerBound: 0.5,
+      upperBound: 1.0,
+    )..repeat(reverse: true); // Creates a pulsing effect
+
+    motionController.addListener(() {
+      setState(() {
+        size = motionController.value * 200; // Scale animation size
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    motionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Container(
-          width: 200,
-          height: 200,
+          width: size,
+          height: size,
           decoration: BoxDecoration(
-            image: DecorationImage(
+            image: const DecorationImage(
               image: AssetImage('lib/assets/images/heart.png'),
               fit: BoxFit.cover,
-              ),
             ),
           ),
         ),
+      ),
     );
   }
 }
